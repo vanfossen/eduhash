b5648fe54253457ec6314d8de2734b00182a1370
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-10-08 12:02:43 -0400

    fix error in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  content: [],
   theme: {
     extend: {},
   },
