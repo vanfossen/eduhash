dee3457e0536a2ed7281ec69687035419904b0a5
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-10-08 12:54:12 -0400

    formatting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex grow items-center justify-center">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
