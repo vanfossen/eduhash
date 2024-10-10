7b3c5b7bc364bca9cefbd32fbf6d325b5be29bd4
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-10-09 20:46:57 -0400

    move content back in App.vue, update login logic, add algo toggle

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,127 @@
 <!-- App.vue -->
 <script setup lang="ts">
 // vue and other libraries
-// TODO
+import { ref } from "vue";
 
-// components
-import Crypto from "./components/Crypto.vue";
-import Login from "./components/Login.vue";
+// constants
+const ALGORITHM_LIST = ["MD5", "scrypt", "Argon2id"];
 
 // variables
-// TODO
+const username = ref("");
+const password = ref("");
+const algorithm = ref("");
+const entry = ref({
+  username: "(no entry)",
+  algorithm: "(no entry)",
+  salt: "(no entry)",
+  hash: "(no entry)",
+});
+const error = ref("");
+
+// function that handles login actions
+const handleLogin = async () => {
+  // TODO - add regex for valid username characters
+  if (!username.value) {
+    error.value = "Username is required.";
+    return;
+  }
+
+  // TODO - add regex for valid password characters
+  if (!password.value) {
+    error.value = "Password is required.";
+    return;
+  }
+
+  // TODO - add regex for valid password characters
+  if (!algorithm.value) {
+    error.value = "Algorithm is required.";
+    return;
+  }
+
+  if (username.value && password.value && algorithm.value) {
+    entry.value = {
+      username: username.value,
+      algorithm: algorithm.value,
+      salt: "TODO",
+      hash: "TODO",
+    };
+
+    username.value = "";
+    password.value = "";
+    algorithm.value = "";
+    error.value = "";
+  }
+};
 </script>
 
 <template>
   <div class="flex grow flex-col items-center justify-center">
-    <Login />
-    <Crypto />
+    <!-- Login.vue -->
+    <div
+      class="my-16 flex flex-col items-center rounded-2xl border border-black px-8 py-4 font-mono shadow-2xl"
+    >
+      <h1 class="mx-2 my-4 text-2xl">Welcome!</h1>
+
+      <!-- username -->
+      <input
+        v-model="username"
+        id="username"
+        type="text"
+        placeholder="Username"
+        class="input input-bordered m-2 w-full max-w-xs"
+      />
+
+      <!-- password -->
+      <input
+        v-model="password"
+        id="password"
+        type="password"
+        placeholder="Password"
+        class="input input-bordered m-2 w-full max-w-xs"
+      />
+
+      <!-- algorithm -->
+      <div class="join my-2">
+        <label v-for="alg in ALGORITHM_LIST" :key="alg">
+          <input
+            v-model="algorithm"
+            class="btn join-item"
+            type="radio"
+            name="options"
+            :value="alg"
+            :aria-label="alg"
+          />
+        </label>
+      </div>
+
+      <!-- Display error message -->
+      <p class="my-2 text-sm text-error" v-if="error">{{ error }}</p>
+
+      <!-- login -->
+      <button class="btn btn-wide my-2" @click="handleLogin">Login</button>
+    </div>
+
+    <!-- Crypto.vue -->
+    <div class="overflow-x-auto rounded-2xl border border-black">
+      <table class="table font-mono">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Algorithm</th>
+            <th>Salt</th>
+            <th>Hash</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>{{ entry.username }}</th>
+            <td>{{ entry.algorithm }}</td>
+            <td>{{ entry.salt }}</td>
+            <td>{{ entry.hash }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
