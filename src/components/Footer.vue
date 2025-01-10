082a8aa17dfda03d7d1848aee178308a317fbf56
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2025-01-10 09:02:13 -0500

    update footer link text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,14 +11,14 @@
         <a
           href="https://github.com/vanfossen/vite-vue-ts-tailwind"
           target="_blank"
-          class="link-hover link link-info"
+          class="link-hover link link-primary"
           >GitHub</a
         >
         with a
         <a
           href="https://github.com/vanfossen/vite-vue-ts-tailwind/blob/main/LICENSE"
           target="_blank"
-          class="link-hover link link-info"
+          class="link-hover link link-primary"
           >MIT license</a
         >.
       </p>
