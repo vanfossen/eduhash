2b7f50dfab17b04ec44dcfe60812352761556cec
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2025-01-14 18:21:35 -0500

    refactor svgs to lucide library

--- src/components/ModalInfo.vue
+++ src/components/ModalInfo.vue
@@ -1,28 +1,17 @@
 <!-- src/components/ModalInfo.vue -->
 <script setup lang="ts">
+// vue and other libraries
+import { Info } from "lucide-vue-next";
+import { ShieldAlert } from "lucide-vue-next";
+import { Database } from "lucide-vue-next";
+
 // components
 import ThemeController from "./ThemeController.vue";
 </script>
 
 <template>
   <button class="btn btn-square btn-ghost" onclick="infoModal.showModal()">
-    <!-- lucide | info (https://lucide.dev/icons/info) -->
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-info"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M12 16v-4" />
-      <path d="M12 8h.01" />
-    </svg>
+    <Info />
   </button>
   <dialog id="infoModal" class="modal">
     <div class="modal-box">
@@ -44,27 +33,9 @@ import ThemeController from "./ThemeController.vue";
         <input type="checkbox" />
         <div class="collapse-title font-bold">
           <span class="inline-flex">
-            <!-- lucide | shield-alert (https://lucide.dev/icons/shield-alert) -->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-shield-alert mr-4 size-6"
-            >
-              <path
-                d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"
-              />
-              <path d="M12 8v4" />
-              <path d="M12 16h.01" />
-            </svg>
-            Important Security Note:</span
-          >
+            <ShieldAlert class="mr-4 size-6" />
+            Important Security Note:
+          </span>
         </div>
         <div class="collapse-content">
           <p>
@@ -82,25 +53,9 @@ import ThemeController from "./ThemeController.vue";
         <input type="checkbox" />
         <div class="collapse-title font-bold">
           <span class="inline-flex">
-            <!-- lucide | database (https://lucide.dev/icons/database) -->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-database mr-4 size-6"
-            >
-              <ellipse cx="12" cy="5" rx="9" ry="3" />
-              <path d="M3 5V19A9 3 0 0 0 21 19V5" />
-              <path d="M3 12A9 3 0 0 0 21 12" />
-            </svg>
-            Local Storage:</span
-          >
+            <Database class="mr-4 size-6" />
+            Local Storage:
+          </span>
         </div>
         <div class="collapse-content">
           <p>
