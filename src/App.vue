0a564eea1f42c48b1fdc1b6be2e2fa6101fcf87b
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2025-01-20 13:00:22 -0500

    add alert, no localStorage memory for close

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <!-- src/App.vue -->
 <script setup lang="ts">
+// vue and other libraries
+import { ref } from "vue";
+import { TriangleAlert } from "lucide-vue-next";
+
 // components
+import Alert from "./components/Alert.vue";
 import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
+
+// variables
+const showAlert = ref<boolean>(true);
 </script>
 
 <template>
   <Navbar />
+
+  <Alert
+    v-if="showAlert"
+    @update:showAlert="showAlert = $event"
+    text="This application is currently in development. Please note that features may not function as expected and are subject to change."
+  >
+    <TriangleAlert />
+  </Alert>
 
   <RouterView />
 
---
+++ src/components/Alert.vue
@@ -0,0 +1,32 @@
+<!-- src/components/Alert.vue -->
+<script setup lang="ts">
+// vue and other libraries
+import { X } from "lucide-vue-next";
+
+// emits
+const emit = defineEmits<{
+  (e: "update:showAlert", value: boolean): void;
+}>();
+
+// props
+const props = defineProps<{
+  text: string;
+}>();
+
+// TODO
+const handleClose = () => {
+  emit("update:showAlert", false);
+};
+</script>
+
+<template>
+  <div role="alert" class="alert alert-warning">
+    <slot />
+    <span>{{ props.text }}</span>
+    <button @click="handleClose" class="btn btn-circle btn-ghost">
+      <X color="black" />
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
