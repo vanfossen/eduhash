165aec11ba439cbbd6742bf6a6a9ea56aeb8f4b1
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2025-02-03 18:34:32 -0500

    update glossary style

--- src/pages/Glossary.vue
+++ src/pages/Glossary.vue
@@ -9,26 +9,28 @@ const glossary = ref(glossaryData);
 </script>
 
 <template>
-  <div class="flex grow flex-col items-center justify-center">
-    <h1 class="text-3xl">Glossary</h1>
+  <div class="grow">
+    <h1 class="m-6 text-center text-3xl">Glossary</h1>
 
-    <div
-      v-for="entry in glossary"
-      :key="entry.word"
-      class="card m-8 w-96 border-white bg-base-100 shadow-xl"
-    >
-      <div class="card-body">
-        <h2 class="card-title">{{ entry.word }}</h2>
-        <p>{{ entry.definition }}</p>
-        <div class="card-actions justify-end">
-          <a
-            v-for="source in entry.sources"
-            :key="source.title"
-            class="link-hover link text-xs"
-            :href="source.url"
-            target="_blank"
-            >({{ source.title }})</a
-          >
+    <div class="flex flex-wrap justify-center">
+      <div
+        v-for="entry in glossary"
+        :key="entry.word"
+        class="card m-4 w-96 border border-black bg-base-100 shadow-xl"
+      >
+        <div class="card-body">
+          <h2 class="card-title">{{ entry.word }}</h2>
+          <p>{{ entry.definition }}</p>
+          <div class="card-actions justify-end">
+            <a
+              v-for="source in entry.sources"
+              :key="source.title"
+              class="link-hover link text-xs"
+              :href="source.url"
+              target="_blank"
+              >({{ source.title }})</a
+            >
+          </div>
         </div>
       </div>
     </div>
